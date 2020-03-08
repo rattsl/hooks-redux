05a5a20fae38683cebee5f2e541e2ca3d89b2124
Hayato Kakiuchi <qweaty.uiop8810@i.softbank.jp>
2020-03-09 02:00:06 +0900

    use useState

--- src/App.js
+++ src/App.js
@@ -1,7 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const App = () => {
-  return <div>Let's practice hooks!</div>;
+  const [count, setCount] = useState(0);
+  const increment = () => setCount(count + 1);
+  const decrement = () => setCount(count - 1);
+  const increment2 = () =>
+    setCount(previousCountState => previousCountState + 1);
+  const decrement2 = () =>
+    setCount(previousCountState => previousCountState - 1);
+  const reset = () => setCount(0);
+  const double = () => setCount(previousCountState => previousCountState * 2);
+  const tree = () =>
+    setCount(previousCountState => {
+      if (previousCountState % 3 === 0) {
+        return previousCountState / 3;
+      } else {
+        return previousCountState;
+      }
+    });
+  return (
+    <React.Fragment>
+      <div>count: {count}</div>
+      <div>
+        <button onClick={increment}>+1</button>
+        <button onClick={decrement}>-1</button>
+      </div>
+      <div>
+        <button onClick={increment2}>+1</button>
+        <button onClick={decrement2}>-1</button>
+      </div>
+      <div>
+        <button onClick={double}>x2</button>
+        <button onClick={tree}>3の商</button>
+      </div>
+      <div>
+        <button onClick={reset}>Reset</button>
+      </div>
+    </React.Fragment>
+  );
 };
 
 export default App;
