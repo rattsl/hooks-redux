4ab18255c6b91f7d011b7d8d92c45584c6f40dd7
Hayato Kakiuchi <qweaty.uiop8810@i.softbank.jp>
2020-03-10 17:42:05 +0900

    use reducer with useReducer

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,50 @@
-import React from 'react';
+import React, { useReducer, useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import reducer from '../reducers';
 
 const App = () => {
+  const [state, dispatch] = useReducer(reducer, []);
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+
+  const addEvent = e => {
+    //画面全体を再レンダするのではなく、更新必要のあるDOMだけ更新する
+    e.preventDefault();
+    //状態をdispatchする
+    dispatch({
+      type: 'CREATE_EVENT',
+      title,
+      body
+    });
+    setBody('');
+    setBody('');
+  };
+  console.log({ state });
   return (
     <div className="container-fluid">
       <h4>イベント作成フォーム</h4>
       <form className="form-group">
         <label htmlFor="formEventTitle">タイトル</label>
-        <input className="form-control" id="formEventTitle" />
+        <input
+          className="form-control"
+          id="formEventTitle"
+          value={title}
+          onChange={e => setTitle(e.target.value)}
+        />
       </form>
       <form className="form-group">
         <label htmlFor="formEventBody">ボディ</label>
-        <textarea className="form-control" id="formEventBody" />
+        <textarea
+          className="form-control"
+          id="formEventBody"
+          value={body}
+          onChange={e => setBody(e.target.value)}
+        />
       </form>
-      <button className="btn btn-primary">イベント作成</button>&nbsp;
+      <button className="btn btn-primary" onClick={addEvent}>
+        イベント作成
+      </button>
+      &nbsp;
       <button className="btn btn-danger">全てのイベントを削除する</button>
       <h4>イベント一覧</h4>
       <table className="table table-hover">
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,36 @@
-export const events = (state = [], action) => {
+//状態をイメージする
+
+// #bafore
+//  state = []
+// #after
+//  state = [
+//    {id: 1, title: "title1", body: "boby1"}
+//  ]
+
+// #bafore
+//  state = [{id: 1, title: "title1", body: "body1"}]
+// #after
+//  state = [
+//    {id: 1, title: "title1", body: "boby1"}
+//    {id: 2, title: "title2", body: "boby2"}
+//  ]
+
+const events = (state = [], action) => {
   switch (action.type) {
     case 'CREATE_EVENT':
-      return state;
+      //どういうイベントが流れ込んでくるか考える
+      const event = { title: action.title, body: action.body };
+      //その時のstateの要素を条件分岐させる
+      const length = state.length;
+      const id = length === 0 ? 1 : state[length - 1].id + 1;
+      return [...state, { id, ...event }];
     case 'DELETE_EVENT':
       return state;
     case 'DELETE_ALL_EVENT':
-      return state;
+      return [];
     default:
       return state;
   }
 };
+
+export default events;
